f42910e565fc19372f6dce0524345fd707dc7c09
gongji370 <66150433+gongji370@users.noreply.github.com>
2022-03-09 19:41:14 +0800

    增加评论

--- src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -13,4 +13,5 @@ public interface DiscussPostMapper {
     int insertDiscussPostRows(DiscussPost discussPost);
 
     DiscussPost selectDiscussPostById(int id);
+    int updateCommentCount(int id,int commentCount);
 }
