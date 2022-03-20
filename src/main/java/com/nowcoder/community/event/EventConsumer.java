6280752ecd530392317d12b541ea69d1a3d8fc62
gongji370 <66150433+gongji370@users.noreply.github.com>
2022-03-20 18:09:17 +0800

    发送系统通知(修改bug)

--- src/main/java/com/nowcoder/community/event/EventConsumer.java
+++ src/main/java/com/nowcoder/community/event/EventConsumer.java
@@ -10,11 +10,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-
+@Component
 public class EventConsumer implements CommunityConstant {
 
     private static final Logger logger= LoggerFactory.getLogger(EventConsumer.class);
